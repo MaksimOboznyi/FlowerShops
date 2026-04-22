6c28fe8a4e613aa0c30d9c8e4e4a3c474d12a666
MaksimObozniy <125642497+MaksimObozniy@users.noreply.github.com>
2026-04-22 21:17:47 +0200

    filtering has been done for the administrator

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, ConsultationRequest
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display= (
+        'id',
+        'customer_name',
+        'phone',
+        'bouquet',
+        'delivery_time',
+        'status',
+        'created_at',
+    )
+    list_filter = (
+        'status',
+        'delivery_time',
+        'created_at',
+    )
+    search_fields = (
+        'customer_name',
+        'phone',
+        'address',
+        'bouquet__title',
+    )
+    list_editable = (
+        'status',
+    )
+    readonly_fields = (
+        'created_at',
+    )
+
+@admin.register(ConsultationRequest)
+class ConsultationRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'customer_name',
+        'phone',
+        'is_processed',
+        'created_at',
+    )
+    list_filter = (
+        'is_processed',
+        'created_at',
+    )
+    search_fields = (
+        'customer_name',
+        'phone',
+    )
+    list_editable = (
+        'ist_processed',
+    )
+    readonly_fields = (
+        'created_at',
+    )
